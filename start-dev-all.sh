1ea7d21f386bdf71baab66ae53725957d8393816
Godwin Pinto <godwin.pinto86@gmail.com>
2023-05-18 16:07:37 +0530

    fix(dependency): fixed the lombok and checked build in docker

--- start-dev-all.sh
+++ start-dev-all.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 clear
 echo "Starting Dev Setup"
-docker-compose -f docker-compose-dev.yml up --no-deps --d --build core-api
+docker-compose -f docker-compose-dev.yml up --d --build core-api
